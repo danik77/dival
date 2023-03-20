ef27ad68703585f54443f9c72309a5f7c5600ed7
Danylo Peretyatko <danik77p@gmail.com>
2023-03-21 00:43:07 +0200

    Edits

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,29 +1,29 @@
 import ContactBox from "../ContactBox";
 import Map from "../Map";
 import BannerBottom from "../Banners/BannerBottom";
-import ReturnButton from '../Map/ReturnButton'
+import ReturnButton from "../Map/ReturnButton";
 import style from "./style.module.scss";
 
 const Footer = ({ contacts, homepage }: any) => {
 	const coords = {
 		lat: contacts.data.attributes.GoogleMapsLatitude,
-		lng: contacts.data.attributes.GoogleMapsLongitude
-	}
+		lng: contacts.data.attributes.GoogleMapsLongitude,
+	};
 
 	return (
 		<>
-		{/*<div className="container"><BannerBottom homepage={homepage}/></div>*/}
-		<div className={style.footer}>
-			<div className={style.footer__box}>
-				<div className={style.footer__left}>
-					<ContactBox contacts={contacts} />
+			{/*<div className="container"><BannerBottom homepage={homepage}/></div>*/}
+			<div className={style.footer}>
+				<div className={style.footer__box}>
+					<div className={style.footer__left}>
+						<ContactBox contacts={contacts} />
+					</div>
+					<div className={style.footer__right}>
+						<Map coords={coords} />
+					</div>
 				</div>
-				<div className={style.footer__right}>
-					<Map coords={coords}/>
+				<div className={style.footer__copyright}><p>{contacts.data.attributes.Copyright}</p></div>
 			</div>
-		
-		</div>	<p>{contacts.data.attributes.Copyright}</p>
-		</div>
 		</>
 	);
 };
