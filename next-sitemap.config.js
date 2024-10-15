600252be340a0b1f8dc1830f0581c268dd1b55a9
Danylo Peretyatko <danik77p@gmail.com>
2024-10-15 22:52:26 +0300

    robots

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: 'https://dival.com.ua',  
+    generateRobotsTxt: false,
+    sitemapSize: 5000, 
+    changefreq: 'daily',
+    priority: 0.7,
+  };
+  
+  module.exports = config;
